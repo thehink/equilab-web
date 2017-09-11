c4ca3621e51e886d670087266edb084657d357ae
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-09-11 13:30:59 +0200

    test commit

---
+++ client/components/hej.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Hej extends Component {
+  render() {
+    return <div>Hej</div>;
+  }
+}
+
+export default Hej;
--- client/components/test.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Logout extends Component {
-  render() {
-    return <div>test</div>;
-  }
-}
-
-export default Logout;
