788551f54194b8457c0291f68742949a52cf9ff4
Benjamin Rizk <benja280@gmail.com>
2017-09-11 19:55:19 +0200

    Disabled semver install script

--- internal/scripts/preinstall.js
+++ internal/scripts/preinstall.js
@@ -12,7 +12,7 @@ var exec = require('child_process').exec;
 var existsSync = require('fs').existsSync;
 var pathResolve = require('path').resolve;
 
-if (existsSync(pathResolve(__dirname, '../../node_modules'))){
+if (existsSync(pathResolve(__dirname, '../../node_modules'))) {
   // An install has already occurred.
   return;
 }
@@ -26,25 +26,27 @@ function checkNodeVersion() {
       'You are currently running Node %s but %s requires %s. Please use a supported version of Node.\n',
       process.version,
       packageJson.name,
-      packageJson.engines.node
+      packageJson.engines.node,
     );
     process.exit(1);
   }
 }
 
 var packageJson = require('../../package.json');
-if (!packageJson.engines
-  || !packageJson.engines.node
-  || !packageJson.devDependencies
-  || !packageJson.devDependencies.semver) {
+if (
+  !packageJson.engines ||
+  !packageJson.engines.node ||
+  !packageJson.devDependencies ||
+  !packageJson.devDependencies.semver
+) {
   // The package has already been customised. Ignore this script.
   return;
 }
 
-exec(
-  'npm install semver@' + packageJson.devDependencies.semver,
-  function installSemverCb(err, stdout, stderr) {
-    if (err) throw err;
-    checkNodeVersion();
-  }
-)
+// exec(
+//   'npm install semver@' + packageJson.devDependencies.semver,
+//   function installSemverCb(err, stdout, stderr) {
+//     if (err) throw err;
+//     checkNodeVersion();
+//   }
+// )
