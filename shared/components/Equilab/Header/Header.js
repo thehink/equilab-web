f20cbfd7c4011e90dc71e13ece4f024ffdaf2212
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-10-04 18:01:02 +0200

    work on header

--- shared/components/Equilab/Header/Header.js
+++ shared/components/Equilab/Header/Header.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
-  render() {
+  renderLinks() {
     return (
       <div>
-        Header
+        <li className="nav-item">
+          <Link to="#" className="nav-link">
+            FAQ
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="#" className="nav-link">
+            Hiring
+          </Link>
+        </li>
       </div>
+    );
+  }
+  render() {
+    return (
+      <nav className="navbar navbar-light">
+        <ul className="nav navbar-nav ml-auto">{this.renderLinks()}</ul>
+      </nav>
     );
   }
 }
