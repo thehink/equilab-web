de01073175c00d202ac2fda1fcb98afb96d807b6
Benjamin Rizk <benja280@gmail.com>
2017-10-05 23:25:17 +0200

    Updated info style

--- shared/components/Equilab/Info/Info.js
+++ shared/components/Equilab/Info/Info.js
@@ -14,32 +14,34 @@ class Info extends Component {
 
     return (
       <div className={`row ${style.wrapper}`}>
-        <div className={'col'}>
-          <div className={style.imageWrapper}>
-            <PreloadImage image={image} alt="mobile" className={style.image1} />
-            <PreloadImage image={image} alt="mobile" className={style.image2} />
+        <div className={`${style.maxWidth}`}>
+          <div className={`hidden-xs-up col-lg-6 col-md-12 ${style.imageCol}`}>
+            <div className={style.imageWrapper}>
+              <PreloadImage image={image} alt="mobile" className={style.image1} />
+              <PreloadImage image={image} alt="mobile" className={style.image2} />
+            </div>
           </div>
-        </div>
 
-        <div className={`col ${style.minHeight}`}>
-          <FormattedMessage id={'Info.what'} defaultMessage={'Vad är Equilab?'}>
-            {txt => <h2>{txt}</h2>}
-          </FormattedMessage>
-          <p className={style.subtitle}>
-            <FormattedMessage id={'Info.what_sub'} defaultMessage={'En bra App!'} />
-          </p>
-          <p className={style.description}>
-            <FormattedMessage
-              id={'Info.what_info'}
-              defaultMessage={
-                'Projektet med hästar är inte så jättekul att hålla på med när man är stressad och hellre gör annat. Men det visade sig fungera hyffsat bra och blev nästan lite kul när jag väl satte igång med det. Tänka sig hur det kan bli när man är röksugen och frustrerad.'
-              }
-            />
-          </p>
+          <div className={`col-lg-6 col-md-12 ${style.minHeight}`}>
+            <FormattedMessage id={'Info.what'} defaultMessage={'Vad är Equilab?'}>
+              {txt => <h2>{txt}</h2>}
+            </FormattedMessage>
+            <p className={style.subtitle}>
+              <FormattedMessage id={'Info.what_sub'} defaultMessage={'En bra App!'} />
+            </p>
+            <p className={style.description}>
+              <FormattedMessage
+                id={'Info.what_info'}
+                defaultMessage={
+                  'Projektet med hästar är inte så jättekul att hålla på med när man är stressad och hellre gör annat. Men det visade sig fungera hyffsat bra och blev nästan lite kul när jag väl satte igång med det. Tänka sig hur det kan bli när man är röksugen och frustrerad.'
+                }
+              />
+            </p>
 
-          <button type="button" className="btn btn-primary">
-            <FormattedMessage id={'Info.sign_up'} defaultMessage={'Sign up!'} />
-          </button>
+            <button type="button" className={`btn ${style.signUpButton}`}>
+              <FormattedMessage id={'Info.sign_up'} defaultMessage={'Sign up!'} />
+            </button>
+          </div>
         </div>
       </div>
     );
