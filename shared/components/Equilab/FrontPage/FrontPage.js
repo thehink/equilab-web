c1b8b78d67629dad2173eed4f7276b77012cbb9c
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-10-07 15:22:01 +0200

    add hero comp

--- shared/components/Equilab/FrontPage/FrontPage.js
+++ shared/components/Equilab/FrontPage/FrontPage.js
@@ -3,12 +3,13 @@ import React, { Component } from 'react';
 import Info from 'Equilab/Info';
 import Testimonials from 'Equilab/Testimonials';
 import AboveFooter from 'Equilab/AboveFooter';
+import Hero from 'Equilab/Hero';
 
 class FrontPage extends Component {
   render() {
     return (
       <div className="front-page">
-        {/*  Hero */}
+        <Hero />
         <Info />
         <Testimonials />
         <AboveFooter />
