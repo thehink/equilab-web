365539a59715aa1e50181ae3f046a7ed00e5f11d
Benjamin Rizk <benja280@gmail.com>
2017-10-09 18:01:11 +0200

    Improved responsive layout

--- shared/components/Equilab/FrontPage/FrontPage.js
+++ shared/components/Equilab/FrontPage/FrontPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Element, scroller } from 'react-scroll';
 
 import Info from 'Equilab/Info';
 import Testimonials from 'Equilab/Testimonials';
@@ -6,9 +7,19 @@ import AboveFooter from 'Equilab/AboveFooter';
 import Hero from 'Equilab/Hero';
 
 class FrontPage extends Component {
+  onClick = () => {
+    scroller.scrollTo('info', {
+      duration: 500,
+      delay: 100,
+      smooth: true,
+      offset: 0,
+    });
+  };
+
   render() {
     return (
       <div className="front-page">
+        <span onClick={this.onClick}>Scroll Test</span>
         <Hero />
         <Info />
         <Testimonials />
