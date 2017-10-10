e1092a850e2e93e567917554527ae925a8522fb4
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-10-10 14:35:31 +0200

    removed scroll example

--- shared/components/Equilab/FrontPage/FrontPage.js
+++ shared/components/Equilab/FrontPage/FrontPage.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Element, scroller } from 'react-scroll';
 
 import Info from 'Equilab/Info';
 import Testimonials from 'Equilab/Testimonials';
@@ -7,19 +6,9 @@ import AboveFooter from 'Equilab/AboveFooter';
 import Hero from 'Equilab/Hero';
 
 class FrontPage extends Component {
-  onClick = () => {
-    scroller.scrollTo('info', {
-      duration: 500,
-      delay: 100,
-      smooth: true,
-      offset: 0,
-    });
-  };
-
   render() {
     return (
       <div className="front-page">
-        <span onClick={this.onClick}>Scroll Test</span>
         <Hero />
         <Info />
         <Testimonials />
