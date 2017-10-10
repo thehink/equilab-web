6b6e06ef2b7798ffc0d62c22c2741dd1d70f6f5f
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-10-10 14:36:02 +0200

    added scroll function

--- shared/components/Equilab/Hero/Hero.js
+++ shared/components/Equilab/Hero/Hero.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { Element, scroller } from 'react-scroll';
 
 import DownloadBar from 'Equilab/DownloadBar';
 import styles from './Hero.scss';
 
 export default class Hero extends Component {
+  onClick = () => {
+    scroller.scrollTo('info', {
+      duration: 500,
+      delay: 100,
+      smooth: true,
+      offset: 0,
+    });
+  };
+
   render() {
     return (
       <div className={`row ${styles.wrapper}`}>
@@ -13,7 +23,9 @@ export default class Hero extends Component {
           <DownloadBar />
           <div className={`${styles.arrow}`}>
             <p>NOT CONVINCED YET?</p>
-            <div className={`${styles.arrowImg}`} />
+            <span onClick={this.onClick}>
+              <div className={`${styles.arrowImg}`} />
+            </span>
           </div>
         </div>
       </div>
